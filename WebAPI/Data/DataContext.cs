32ab691eac8c58be7cca09be10abf3bb9d1803d9
Muhammad Rohadi <muhammadrohadi03@gmail.com>
2020-12-06 16:01:40 +0700

    Walking skleton api 1

---
+++ WebAPI/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Entities;
+
+namespace WebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
