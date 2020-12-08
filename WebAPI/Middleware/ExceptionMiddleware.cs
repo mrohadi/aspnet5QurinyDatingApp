3703bb473d3ae2070f0c0bde8b60d6ba86e0bbb8
Muhammad Rohadi <muhammadrohadi03@gmail.com>
2020-12-08 21:47:01 +0700

    Error handling

---
+++ WebAPI/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using WebAPI.Errors;
+
+namespace WebAPI.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+        public ExceptionMiddleware(
+            RequestDelegate next, 
+            ILogger<ExceptionMiddleware> logger, 
+            IHostEnvironment env)
+        {
+                _env = env;
+                _logger = logger;
+                _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                httpContext.Response.ContentType = "application/json";
+                httpContext.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+
+                var response = _env.IsDevelopment()
+                    ? new ApiException(httpContext.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                    : new ApiException(httpContext.Response.StatusCode, "Internet Server Error");
+
+                var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await httpContext.Response.WriteAsync(json);
+            }
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,17 +1,10 @@
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using WebAPI.Data;
 using WebAPI.Extensions;
-using WebAPI.Interfaces;
-using WebAPI.Services;
+using WebAPI.Middleware;
 
 namespace WebAPI
 {
@@ -42,12 +35,14 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
-            }
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            //     app.UseSwagger();
+            //     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+            // }
+
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
 
